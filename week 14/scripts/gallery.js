818692de77333af081c99c75d83b0354283292ee
Bella <bella.kamala@gmail.com>
2022-11-13 23:10:55 +0300

    week 14

---
+++ week 14/scripts/gallery.js
@@ -0,0 +1,10 @@
+function forwardTo() {
+    let lionImage = document.getElementById('lionImage');
+    lionImage.src = "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg";
+
+}
+
+function returnTo() {
+    let lionImage = document.getElementById('lionImage');
+    lionImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/African_lion%2C_Panthera_leo_at_Kgalagadi_Transfrontier_Park%2C_Northern_Cape%2C_South_Africa_%2834637890821%29.jpg/1920px-African_lion%2C_Panthera_leo_at_Kgalagadi_Transfrontier_Park%2C_Northern_Cape%2C_South_Africa_%2834637890821%29.jpg"
+}
