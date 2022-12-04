0524e2b426625b81c181316065fc3386a129a604
Bella <bella.kamala@gmail.com>
2022-12-04 17:00:00 +0300

    task1

---
+++ week 17/scripts/task1.js
@@ -0,0 +1,35 @@
+"use strict"
+
+let button = document.querySelector("button");
+button.addEventListener("click", sortName);
+//button.addEventListener("click", checkName);
+
+
+function sortName() {
+    let text = document.querySelector("input").value;
+    let cleantext = text.trim();
+    let arr = cleantext.split(" ");
+    let lastname = arr[0];
+    let name = arr[1];
+    let patr = arr[2];
+    let lastnameOK = lastname[0].toUpperCase() + lastname.slice(1).toLowerCase();
+    let nameOK = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+
+    document.querySelector("#lastname").innerHTML = "Фамилия: " + lastnameOK;
+    document.querySelector("#name").innerHTML = "Имя: " + nameOK;
+
+    if (arr.length == 2) {
+        document.querySelector("#patr").innerHTML = "Отчество: нет";
+    } else {
+        let patrOK = patr[0].toUpperCase() + patr.slice(1).toLowerCase();
+        document.querySelector("#patr").innerHTML = "Отчество: " + patrOK;
+    };
+
+};
+
+
+
+
+
+
---
+++ week 17/scripts/task2.js
@@ -0,0 +1,8 @@
+let comment = document.querySelector("textarea").value;
+
+
+function addComment() {
+
+    document.querySelector("#comment").innerHTML = comment;
+
+}
